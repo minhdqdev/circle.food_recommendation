c5ab3625529cf86daa2825065116bf20dd95ee26
minhdq99hp <minhdq99hp@gmail.com>
2020-10-20 20:27:01 +0700

    add dummy screens

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,119 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from "@expo/vector-icons";
+
+
+class HomeScreen extends React.Component {
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+
+class ActivityScreen extends React.Component {
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Activity</Text>
+      </View>
+    );
+  }
+}
+
+
+class CheckinScreen extends React.Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Checkin</Text>
+      </View>
+    );
+  }
+}
+
+
+class SavedScreen extends React.Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Saved</Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Profile</Text>
+      </View>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Tab = createBottomTabNavigator();
+
+const routeIcons = {
+  Home: "home",
+  Activity: "profile",
+  Checkin: "pluscircleo",
+  Saved: "staro",
+  Profile: "user",
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name={routeIcons[route.name]}
+              size={24}
+              color={focused ? "red" : "grey"}
+            />
+          ),
+        })}
+        tabBarOptions={{
+          activeTintColor: "red",
+          inactiveTintColor: "grey",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Activity" component={ActivityScreen}/>
+        <Tab.Screen name="Checkin" component={CheckinScreen}/>
+        <Tab.Screen name="Saved" component={SavedScreen}/>
+        <Tab.Screen name="Profile" component={ProfileScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ frontend/README.md
@@ -0,0 +1,14 @@
+# Frontend
+
+## Setup
+```bash
+expo init frontend
+# choose blank
+
+# install react-navigation
+npm install @react-navigation/native
+
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+npm install @react-navigation/stack
+```
